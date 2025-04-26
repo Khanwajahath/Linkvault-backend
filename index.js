3d5c460bdbf0462c787d3890406ed39430be1531
WajahathKhan03 <wajjakhan1526@gmail.com>
2025-04-27 00:18:13 +0530

    added cors settings

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-
+app.use(cors({
+  origin: 'https://linkvault-frontend.vercel.app',
+  credentials: true
+}));
 // MongoDB Connection
 const MONGO_URI = process.env.MONGODB_URI;
 console.log(MONGO_URI);
